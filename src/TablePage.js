b1bebb0e20e11f2961c0604b1f18e202205a3afc
Daniil Loyko <loikozavr@yandex.ru>
2022-04-11 03:21:45 +0300

    добавлена сортировка в таблицах + исправлена ширина столбцов

--- src/TablePage.js
+++ src/TablePage.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import {Table} from 'react-bootstrap';
-import { useDispatch, useSelector} from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { selectRow } from './redux/actions';
 import LoadingOverlay from './LoadingOverlay';
 import './styles/Navigation.css';
+import './styles/TablePage.css';
+import { SortDirection } from './redux/enums';
+import { render } from '@testing-library/react';
 
 function TablePage() {
 
@@ -13,48 +16,82 @@ function TablePage() {
 
 
     const isLoading = useSelector(state => {
-        const {navigationReducer} = state;
+        const { navigationReducer } = state;
         return navigationReducer.isLoading;
     });
     const currentTable = useSelector(state => {
-        const {navigationReducer} = state;
+        const { navigationReducer } = state;
         return navigationReducer.currentTable;
     });
     const tables = useSelector(state => {
-        const {navigationReducer} = state;
+        const { navigationReducer } = state;
         return navigationReducer.tables;
     });
+
+    const [sortedData, setSortedData] = useState(currentTable?.Data);
+    const [sortInfo, setSortInfo] = useState({ sortAttr: "", sortDirection: SortDirection.NONE });
 
     const handleRowEdit = (row) => {
         dispatch(selectRow(row));
         navigate('/view_row');
     };
-    const handleAddRow = () => {
+    const handleAddRow = (e) => {
+        e.preventDefault();
+
         navigate('/add_row');
     };
 
-    return(
+    const handleSort = (attrName, attrType) => {
+        if (sortInfo.sortAttr === attrName && sortInfo.sortDirection === SortDirection.DOWN) {
+            if (attrType === 'date') {
+                setSortedData(currentTable?.Data.sort((a, b) => (new Date(a[attrName]).getTime() < new Date(b[attrName]).getTime()) ? 1 : ((new Date(b[attrName]).getTime() < new Date(a[attrName]).getTime()) ? -1 : 0)));
+            } else {
+                setSortedData(currentTable?.Data.sort((a, b) => (a[attrName] < b[attrName]) ? 1 : ((b[attrName] < a[attrName]) ? -1 : 0)));
+            }
+            setSortInfo({ sortAttr: attrName, sortDirection: SortDirection.UP })
+        } else {
+            if (attrType === 'date') {
+                setSortedData(currentTable?.Data.sort((a, b) => (new Date(a[attrName]).getTime() > new Date(b[attrName]).getTime()) ? 1 : ((new Date(b[attrName]).getTime() > new Date(a[attrName]).getTime()) ? -1 : 0)));
+            } else {
+                setSortedData(currentTable?.Data.sort((a, b) => (a[attrName] > b[attrName]) ? 1 : ((b[attrName] > a[attrName]) ? -1 : 0)));
+            }
+            setSortInfo({ sortAttr: attrName, sortDirection: SortDirection.DOWN })
+        }
+    };
+
+    useEffect(() => {
+        setSortedData(currentTable?.Data);
+        setSortInfo({ sortAttr: "", sortDirection: SortDirection.NONE })
+    }, [currentTable]);
+
+    return (
 
         <>
             <LoadingOverlay show={isLoading} />
-            {JSON.stringify(currentTable) !== JSON.stringify({}) ? 
-                tables.length > 0 ? 
+            {JSON.stringify(currentTable) !== JSON.stringify({}) ?
+                tables.length > 0 ?
                     <div className='content-tabs'>
-                        <button className='def-btn add-btn' onClick={() => handleAddRow()}>Добавить</button>
-                        <Table className='content' striped bordered hover>
+                        <button className='def-btn add-btn' onClick={(e) => handleAddRow(e)}>Добавить</button>
+                        <Table className='custom-tbl' striped bordered hover >
                             <thead>
-                                <tr>
-                                    {currentTable?.Attributes.sort((a,b) => (a.Ord > b.Ord) ? 1 : ((b.Ord > a.Ord) ? -1 : 0)).map((attr)=>
-                                        attr.PkFlag === 0 && <th key={attr.AttrId}>{attr.AttrName}</th>
+                                <tr >
+                                    {currentTable?.Attributes.sort((a, b) => (a.Ord > b.Ord) ? 1 : ((b.Ord > a.Ord) ? -1 : 0)).map((attr) =>
+                                        attr.PkFlag === 0 &&
+                                        <th key={attr.AttrId} onClick={() => handleSort(attr.SystemAttrName, attr.SystemAttrTypeName)} className='attr-header-cell' >
+                                            <div className='attr-header-div'>
+                                                <div>{attr.AttrName}</div>
+                                                <div className='sort-arrow'>{sortInfo.sortAttr === attr.SystemAttrName ? sortInfo.sortDirection : <>&ensp;</>}</div>
+                                            </div>
+                                        </th>
                                     )}
                                 </tr>
                             </thead>
                             <tbody>
-                                {currentTable?.Data?.map((row, rowIdx)=>
+                                {sortedData?.map((row, rowIdx) =>
                                     <tr key={rowIdx} className='row' onClick={() => handleRowEdit(row)}>
-                                        {row && currentTable.Attributes.sort((a,b) => (a.Ord > b.Ord) ? 1 : ((b.Ord > a.Ord) ? -1 : 0)).map((attr)=>
+                                        {row && currentTable.Attributes.sort((a, b) => (a.Ord > b.Ord) ? 1 : ((b.Ord > a.Ord) ? -1 : 0)).map((attr) =>
                                             attr.PkFlag === 0 &&
-                                            <td key={attr.AttrId}>
+                                            <td key={attr.AttrId} className='attr-cell'>
                                                 {attr.AttrTypeName === 'связь' ? row[attr.AttrTypeProp2] : row[attr.SystemAttrName]}
                                             </td>
                                         )}
--- src/redux/enums.js
+++ src/redux/enums.js
@@ -1,2 +1,3 @@
 export const SettingsMenu = { CONF: 'Конфигурация', USERS: 'Пользователя', ROLES: 'Роли' };
 export const ViewRowMods = { ADD: 'add', VIEW: 'view' };
+export const SortDirection = { UP: '↑', DOWN: '↓', NONE: 'none' };
